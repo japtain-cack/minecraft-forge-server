1bb3c2564d7f8e0ea2e86ecc60e46434d53986d9
Nathan Snow <nathan.snow@mimir-tech.org>
2021-06-08 19:00:28 +0000

    updating entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -9,17 +9,16 @@ echo "Forge server version set to: $FORGE_VERSION"
 echo
 
 sudo chown -R minecraft:minecraft ${MC_HOME}
-git config --global --unset core.autocrlf
 
 # Auto updates
 if [ -f "${MC_HOME}/server/forge-${FORGE_VERSION}-installer.jar" ]; then
   echo "Existing Forge jar found: forge-${FORGE_VERSION}-installer.jar"
 else
-  echo -e "\nForge set to latest or no correct forge version found."
+  echo -e "\nNo version for forge ${FORGE_VERSION} found."
   echo -e "Compiling new binarys.\n"
-  curl -o forge-instaler.jar https://maven.minecraftforge.net/net/minecraftforge/forge/${FORGE_VERSION}/forge-${FORGE_VERSION}-installer.jar && \
+  curl -o forge-installer.jar https://maven.minecraftforge.net/net/minecraftforge/forge/${FORGE_VERSION}/forge-${FORGE_VERSION}-installer.jar && \
     chmod +x *.jar && \
-    java -Xmx512M -jar forge-installer.jar --installServer --extract ${MC_HOME}/server
+    java -Xmx512M -jar forge-installer.jar --installServer ${MC_HOME}/server
 fi
 
 # Launch the specified version
